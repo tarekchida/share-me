d70e25a95ca41e4e09c42379da97cf5db8712a95
Tarek chida <tarek.chida@gmail.com>
2014-10-30 22:13:42 +0100

    package update 1.1.0

--- class.shareMe.php
+++ class.shareMe.php
@@ -2,8 +2,15 @@
 
 class shareMe {
 
+    static $popinWidth = 545;
+    static $popinHight = 433;
+
+    /*
+     *  PLUGIN INSTALL 
+     */
+
     public static function sm_activation() {
-        
+
         global $wpdb;
         $table = SM_TABLE_PREFIX . "social_list";
         $structure = "CREATE TABLE  IF NOT EXISTS  $table (
@@ -24,7 +31,6 @@ class shareMe {
         $wpdb->query("INSERT INTO $table  (`id`, `name`, `status`) VALUES (NULL, 'blogger', '1');");
 
 
-
         $table = SM_TABLE_PREFIX . "config";
         $structure = "CREATE TABLE  IF NOT EXISTS  $table (
         id INT(9) NOT NULL AUTO_INCREMENT,
@@ -38,6 +44,10 @@ class shareMe {
         $wpdb->query("INSERT INTO $table  (`id`, `theme`, `h_pos`, `v_pos`, `size`) VALUES (NULL, 'cercle', 'left' , 'up', 32);");
     }
 
+    /*
+     *  PLUGIN UNINSTALL 
+     */
+
     public static function sm_deactivation() {
 
         global $wpdb;
@@ -48,6 +58,123 @@ class shareMe {
         $table = SM_TABLE_PREFIX . "social_list";
         $structure = "drop table if exists $table";
         $wpdb->query($structure);
+    }
+
+    /*
+     * Add plugin's Css and Js to head
+     */
+
+    public static function sm_add_style_script() {
+        wp_enqueue_style('sm_style', plugins_url('css/style.css', __FILE__));
+        wp_enqueue_script('sm_script', plugins_url('/js/scripts.js', __FILE__));
+    }
+
+    public static function sm_getSocialShare($content) {
+
+        global $wpdb;
+        $sm_theme = "circle";
+        $sm_h_pos = "right";
+        $sm_v_pos = "up";
+        $sm_size = "32";
+        $socialList = array();
+
+        $sql = "SELECT *FROM " . SM_TABLE_PREFIX . "config where  1";
+        $data = $wpdb->get_results($sql);
+        foreach ($data as $item) {
+
+            $sm_theme = $item->theme;
+            $sm_h_pos = $item->h_pos;
+            $sm_v_pos = $item->v_pos;
+            $sm_size = $item->size;
+        }
+        $sql = "SELECT *FROM " . SM_TABLE_PREFIX . "social_list where  1";
+        $socials = $wpdb->get_results($sql);
+        if (count($socials) > 0) {
+            foreach ($socials as $social) {
+                $socialList[] = $social;
+            }
+        }
+
+        $path = SM_URL . '/images/' . $sm_theme . '/';
+
+        $shares = self::sm_createShareButtons($socialList, $path, $sm_h_pos, $sm_size);
+
+        if ($sm_v_pos == 'up') {
+            return $shares . $content;
+        } else {
+            return $content . $shares;
+        }
+    }
+
+    /*
+     * GENERATE SHARE BUTTONS
+     */
+
+    public static function sm_createShareButtons($socialList, $path, $sm_h_pos, $sm_size) {
+
+        $shares = "<div id='share-me'><ul class='share-" . $sm_h_pos . "'>";
+        foreach ($socialList as $item) {
+
+            if ($item->status == 0) {
+                continue;
+            }
+
+            $shares.='<li>';
+            $shares.=self::sm_getLink($item->name);
+            $shares.='<img alt = "" src = "' . $path . $item->name . '.png"  height="' . $sm_size . 'px"  width="' . $sm_size . 'px">';
+            $shares.='</a>';
+            $shares.='</li>';
+        }
+        return $shares .= "</ul></div><br/>";
+    }
+
+    public static function sm_getLink($type) {
+        global $post;
+        switch ($type) {
+            case 'facebook': return '<a href="http://www.facebook.com/sharer.php?u=' . apply_filters("the_permalink", get_permalink()) . '&t=' . urlencode(get_the_title()) . '" alt="Share on Facebook" title="Share on Facebook"   onclick="return smWindowpop(this.href,' . self::$popinWidth . ',' . self:: $popinHight . ')">';
+                break;
+            case 'twitter': return '<a href="http://twitter.com/share?text=' . urlencode(get_the_title()) . '-&url=' . apply_filters("the_permalink", get_permalink()) . '&via=StadeFrance" alt="Tweet This Post" title="Tweet This Post"  onclick="return smWindowpop(this.href,' . self::$popinWidth . ',' . self::$popinHight . ')">';
+                break;
+            case 'googleplus': return '<a href="https://plusone.google.com/_/+1/confirm?hl=fr-FR&url=' . apply_filters("the_permalink", get_permalink()) . '" alt="Share on Google+" title="Share on Google+"  target="_blank"onclick="return smWindowpop(this.href,' . self::$popinWidth . ',' . self::$popinHight . ')">';
+                break;
+            case 'tumblr': $thumbID = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large');
+                return '<a href="http://www.tumblr.com/share/photo?source=' . urlencode($thumbID[0]) . '&caption=' . urlencode(get_the_title()) . '&clickthru=' . urlencode(get_permalink()) . '" title="Share on Tumblr"  onclick="return smWindowpop(this.href,' . self::$popinWidth . ',' . self::$popinHight . ')"> ';
+                break;
+            case 'linkedin': return '<a href="http://www.linkedin.com/shareArticle?mini=true&url=' . apply_filters("the_permalink", get_permalink()) . '&title=' . urlencode(get_the_title()) . '&source=Stadefrance" onclick="return smWindowpop(this.href, ' . self::$popinWidth . ',' . self::$popinHight . ')">';
+                break;
+            default: return '';
+                break;
+        }
+    }
+
+    /*
+     * Set image in hrader for pot share
+     */
+
+    public static function sm_get_post_image() {
+        global $post;
+
+        $args = array(
+            'post_type' => 'attachment',
+            'post_mime_type' => 'image',
+            'post_parent' => $post->ID
+        );
+        $images = get_posts($args);
+        $src = array();
+        foreach ($images as $image) {
+            $src[] = wp_get_attachment_url($image->ID, array(120, 120));
+        }
+        if ($src) {
+            $postImage = $src [0];
+        }
+        if (empty($postImage)) {
+            $postImage = SM_URL . 'css/images/logo_big.png';
+        }
+        echo '<meta property="og:image" content="' . $postImage . '"/>';
+    }
+
+    public static function sm_admin_menu() {
+        add_menu_page('Share Me', 'Share Me', 'manage_options', 'share-me/admin-share-me.php', '', plugins_url('css/images/logo_small.png', __FILE__), 100);
     }
 
 }
--- share-me.php
+++ share-me.php
@@ -9,12 +9,11 @@
   Description: Share WP posts on multiple Social Networks with different icons sets
   Author: Tarek Chida
   Author URI: http://tarek-chida.url.ph/
-  Version: 1.0.2
+  Version: 1.1.0
  */
 
- 
-define('SM_FOLDER', dirname(plugin_basename(__FILE__))); 
-define('SM_URL', plugin_dir_url( __FILE__ ) );
+define('SM_FOLDER', dirname(plugin_basename(__FILE__)));
+define('SM_URL', plugin_dir_url(__FILE__));
 define('SM_FILE_PATH', plugin_dir_path(__FILE__));
 define('SM_THEMES_PATH', SM_FILE_PATH . '/images/');
 
@@ -22,126 +21,13 @@ global $wpdb;
 $pro_table_prefix = $wpdb->prefix . 'sm_';
 define('SM_TABLE_PREFIX', $pro_table_prefix);
 
-add_action('wp_enqueue_scripts', 'sm_add_style_script');
-
-function sm_add_style_script() {
-    wp_enqueue_style('sm_style', plugins_url('css/style.css', __FILE__));
-    wp_enqueue_script('sm_script', plugins_url('/js/scripts.js', __FILE__));
-}
-
-
+//plugin install/uninstall
 register_activation_hook(__FILE__, array('shareMe', 'sm_activation'));
 register_deactivation_hook(__FILE__, array('shareMe', 'sm_deactivation'));
 
 require_once( SM_FILE_PATH . 'class.shareMe.php' );
 
-
-
-
-add_action('admin_menu', 'sm_admin_menu');
-
-function sm_admin_menu() {
-
-    add_menu_page('Share Me', 'Share Me', 'manage_options', 'share-me/admin-share-me.php', '', plugins_url('css/images/logo_small.png', __FILE__), 100);
-}
-
-function sm_getSocialShare($content) {
-
-    global $wpdb;
-    $sm_theme = "circle";
-    $sm_h_pos = "right";
-    $sm_v_pos = "up";
-    $sm_size = "32";
-    $social_list = array();
-
-    $sql = "SELECT *FROM " . SM_TABLE_PREFIX . "config where  1";
-    $data = $wpdb->get_results($sql);
-    foreach ($data as $item) {
-
-        $sm_theme = $item->theme;
-        $sm_h_pos = $item->h_pos;
-        $sm_v_pos = $item->v_pos;
-        $sm_size = $item->size;
-    }
-
-
-
-    $sql = "SELECT *FROM " . SM_TABLE_PREFIX . "social_list where  1";
-    $socials = $wpdb->get_results($sql);
-    if (count($socials) > 0) {
-        foreach ($socials as $social) {
-            $social_list[] = $social;
-        }
-    }
-
-
-
-    $path = SM_URL . '/images/' . $sm_theme . '/';
-    $shares = "<div id='share-me'><ul class='share-" . $sm_h_pos . "'>";
-
-
-    foreach ($social_list as $item) {
-
-        if ($item->status == 0)
-            continue;
-
-        $shares.='<li>';
-
-        $shares.=sm_getLink($item->name);
-        $shares.='<img alt = "" src = "' . $path . $item->name . '.png"  height="' . $sm_size . 'px"  width="' . $sm_size . 'px">';
-        $shares.='</a>';
-        $shares.='</li>';
-    }
-    $shares .= "</ul></div><br/>";
-    if ($sm_v_pos == 'up') {
-        return $shares . $content;
-    } else {
-        return $content . $shares;
-    }
-}
-
-add_filter('the_content', 'sm_getSocialShare');
-
-function sm_getLink($type) {
-    global $post;
-    switch ($type) {
-        case 'facebook': return '<a href="http://www.facebook.com/sharer.php?u=' . apply_filters("the_permalink", get_permalink()) . '&t=' . urlencode(get_the_title()) . '" alt="Share on Facebook" title="Share on Facebook"   onclick="return smWindowpop(this.href, 545, 433)">';
-            break;
-        case 'twitter': return '<a href="http://twitter.com/share?text=' . urlencode(get_the_title()) . '-&url=' . apply_filters("the_permalink", get_permalink()) . '&via=StadeFrance" alt="Tweet This Post" title="Tweet This Post"  onclick="return smWindowpop(this.href, 545, 433)">';
-            break;
-        case 'googleplus': return '<a href="https://plusone.google.com/_/+1/confirm?hl=fr-FR&url=' . apply_filters("the_permalink", get_permalink()) . '" alt="Share on Google+" title="Share on Google+"  target="_blank"onclick="return smWindowpop(this.href, 545, 433)">';
-            break;
-        case 'tumblr': $thumbID = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large');
-            return '<a href="http://www.tumblr.com/share/photo?source=' . urlencode($thumbID[0]) . '&caption=' . urlencode(get_the_title()) . '&clickthru=' . urlencode(get_permalink()) . '" title="Share on Tumblr"  onclick="return smWindowpop(this.href, 545, 433)"> ';
-            break;
-        case 'linkedin': return '<a href="http://www.linkedin.com/shareArticle?mini=true&url=' . apply_filters("the_permalink", get_permalink()) . '&title=' . urlencode(get_the_title()) . '&source=Stadefrance" onclick="return smWindowpop(this.href, 545, 433)">';
-            break;
-        default: return '';
-            break;
-    }
-}
-
-function sm_get_post_image() {
-    global $post;
-
-    $args = array(
-        'post_type' => 'attachment',
-        'post_mime_type' => 'image',
-        'post_parent' => $post->ID
-    );
-    $images = get_posts($args);
-    $src = array();
-    foreach ($images as $image) {
-        $src[] = wp_get_attachment_url($image->ID, array(120, 120));
-    }
-    if ($src) {
-        $postImage = $src [0];
-    }
-
-    if (empty($postImage)) {
-        $postImage = SM_URL . '/css/images/logo_big.png';
-    }
-    echo '<meta property="og:image" content="' . $postImage . '"/>';
-}
-
-add_action('wp_head', 'sm_get_post_image', 5);
+add_action('wp_enqueue_scripts', array('shareMe', 'sm_add_style_script') );
+add_action('admin_menu', array('shareMe', 'sm_admin_menu'));
+add_filter('the_content', array('shareMe', 'sm_getSocialShare'));
+add_action('wp_head', array('shareMe', 'sm_get_post_image'), 5);
